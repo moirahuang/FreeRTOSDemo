102a004aa8ee09d0d82c7b1b7949cb34db4ffe37
moirahuang <huang.moira@gmail.com>
2019-07-30 15:33:03 -0700

    formatting

--- demos/ti/cc3220_launchpad/common/application_code/AdafruitAdaptor.cpp
+++ demos/ti/cc3220_launchpad/common/application_code/AdafruitAdaptor.cpp
@@ -67,58 +67,59 @@ Adafruit_TMP006 tmp006(0x41); // start with a different i2c address!
    Intel(R) Curie(TM) module can be used to read gyroscope data
 */
 
-float convertRawGyro(int gRaw) {
-  // since we are using 250 degrees/seconds range
-  // -250 maps to a raw value of -32768
-  // +250 maps to a raw value of 32767
+float convertRawGyro(int gRaw)
+{
+    // since we are using 250 degrees/seconds range
+    // -250 maps to a raw value of -32768
+    // +250 maps to a raw value of 32767
 
-  float g = (gRaw * 250.0) / 32768.0;
+    float g = (gRaw * 250.0) / 32768.0;
 
-  return g;
+    return g;
 }
 
-void setup() {
-  Serial.begin(9600); // initialize Serial communication
-//  while (!Serial);    // wait for the serial port to open
+void setup()
+{
+    Serial.begin(9600); // initialize Serial communication
+                        //  while (!Serial);    // wait for the serial port to open
 
-  // initialize device
-  Serial.println("Initializing IMU device...");
-//  BMI160.begin(BMI160GenClass::SPI_MODE, /* SS pin# = */10);
-  BMI160.begin(BMI160GenClass::I2C_MODE);
-  uint8_t dev_id = BMI160.getDeviceID();
-  Serial.print("DEVICE ID: ");
-  Serial.println(dev_id, HEX);
+    // initialize device
+    Serial.println("Initializing IMU device...");
+    //  BMI160.begin(BMI160GenClass::SPI_MODE, /* SS pin# = */10);
+    BMI160.begin(BMI160GenClass::I2C_MODE);
+    uint8_t dev_id = BMI160.getDeviceID();
+    Serial.print("DEVICE ID: ");
+    Serial.println(dev_id, HEX);
 
-   // Set the accelerometer range to 250 degrees/second
-  BMI160.setGyroRange(250);
-  Serial.println("Initializing IMU device...done.");
+    // Set the accelerometer range to 250 degrees/second
+    BMI160.setGyroRange(250);
+    Serial.println("Initializing IMU device...done.");
 }
 
-void loop() {
-  int gxRaw, gyRaw, gzRaw;         // raw gyro values
-  float gx, gy, gz;
+void loop()
+{
+    int gxRaw, gyRaw, gzRaw; // raw gyro values
+    float gx, gy, gz;
 
-  // read raw gyro measurements from device
-  BMI160.readGyro(gxRaw, gyRaw, gzRaw);
+    // read raw gyro measurements from device
+    BMI160.readGyro(gxRaw, gyRaw, gzRaw);
 
-  // convert the raw gyro data to degrees/second
-  gx = convertRawGyro((int) gxRaw);
-  gy = convertRawGyro((int) gyRaw);
-  gz = convertRawGyro((int) gzRaw);
+    // convert the raw gyro data to degrees/second
+    gx = convertRawGyro((int)gxRaw);
+    gy = convertRawGyro((int)gyRaw);
+    gz = convertRawGyro((int)gzRaw);
 
-  // display tab-separated gyro x/y/z values
-  Serial.print("g:\t");
-  Serial.print(gx);
-  Serial.print("\t");
-  Serial.print(gy);
-  Serial.print("\t");
-  Serial.print(gz);
-  Serial.println();
+    // display tab-separated gyro x/y/z values
+    Serial.print("g:\t");
+    Serial.print(gx);
+    Serial.print("\t");
+    Serial.print(gy);
+    Serial.print("\t");
+    Serial.print(gz);
+    Serial.println();
 
-//  delay(500);
+    //  delay(500);
 }
-
-
 
 /*
    Copyright (c) 2016 Intel Corporation.  All rights reserved.
@@ -135,10 +136,10 @@ void loop() {
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-void SensorsLoop( void * context )
+void SensorsLoop(void *context)
 {
     setup();
-    for (; ;)
+    for (;;)
     {
         loop();
         sleep(1);
--- demos/ti/cc3220_launchpad/common/application_code/BMI160Gen.cpp
+++ demos/ti/cc3220_launchpad/common/application_code/BMI160Gen.cpp
@@ -12,7 +12,8 @@ bool BMI160GenClass::begin(const int spi_cs_pin, const int intr_pin)
 bool BMI160GenClass::begin(Mode mode, const int arg1, const int arg2)
 {
     this->mode = mode;
-    switch (this->mode) {
+    switch (this->mode)
+    {
     case INVALID_MODE:
         return false;
     case I2C_MODE:
@@ -24,7 +25,8 @@ bool BMI160GenClass::begin(Mode mode, const int arg1, const int arg2)
     default:
         return false;
     }
-    if (0 <= arg2) {
+    if (0 <= arg2)
+    {
 //        interrupt_pin = digitalPinToInterrupt(arg2);
 #ifdef DEBUG
         Serial.print("BMI160GenClass::begin(): pin#=");
@@ -39,22 +41,26 @@ bool BMI160GenClass::begin(Mode mode, const int arg1, const int arg2)
 void BMI160GenClass::attachInterrupt(void (*callback)(void))
 {
     CurieIMUClass::attachInterrupt(NULL);
-    if (0 <= interrupt_pin) {
-//        ::attachInterrupt(interrupt_pin, callback, FALLING);
-    } else {
+    if (0 <= interrupt_pin)
+    {
+        //        ::attachInterrupt(interrupt_pin, callback, FALLING);
+    }
+    else
+    {
         Serial.println("BMI160GenClass::attachInterrupt: No valid interruption pin.");
     }
 }
 
 void BMI160GenClass::ss_init()
 {
-    switch (this->mode) {
+    switch (this->mode)
+    {
     case I2C_MODE:
         i2c_init();
         break;
-//    case SPI_MODE:
-//        spi_init();
-//        break;
+        //    case SPI_MODE:
+        //        spi_init();
+        //        break;
     default:
         break;
     }
@@ -62,13 +68,14 @@ void BMI160GenClass::ss_init()
 
 int BMI160GenClass::ss_xfer(uint8_t *buf, unsigned tx_cnt, unsigned rx_cnt)
 {
-    switch (this->mode) {
+    switch (this->mode)
+    {
     case I2C_MODE:
         return i2c_xfer(buf, tx_cnt, rx_cnt);
-//    case SPI_MODE:
-//        if (rx_cnt) /* For read transfers, assume 1st byte contains register address */
-//            buf[0] |= (1 << BMI160_SPI_READ_BIT);
-//        return spi_xfer(buf, tx_cnt, rx_cnt);
+        //    case SPI_MODE:
+        //        if (rx_cnt) /* For read transfers, assume 1st byte contains register address */
+        //            buf[0] |= (1 << BMI160_SPI_READ_BIT);
+        //        return spi_xfer(buf, tx_cnt, rx_cnt);
     default:
         return 0;
     }
@@ -77,65 +84,70 @@ int BMI160GenClass::ss_xfer(uint8_t *buf, unsigned tx_cnt, unsigned rx_cnt)
 void BMI160GenClass::i2c_init()
 {
 #ifdef DEBUG
-  Serial.println("BMI160GenClass::i2c_init()...");
+    Serial.println("BMI160GenClass::i2c_init()...");
 #endif // DEBUG
 
-  Wire.begin();
-  Wire.beginTransmission(i2c_addr);
-  if( Wire.endTransmission() != 0 )
-      Serial.println("BMI160GenClass::i2c_init(): I2C failed.");
+    Wire.begin();
+    Wire.beginTransmission(i2c_addr);
+    if (Wire.endTransmission() != 0)
+        Serial.println("BMI160GenClass::i2c_init(): I2C failed.");
 
 #ifdef DEBUG
-  int id = getDeviceID();
-  Serial.print("BMI160GenClass::i2c_init(): CHIP ID = 0x");
-  Serial.println(id, HEX);
-  Serial.println("BMI160GenClass::i2c_init()...done");
+    int id = getDeviceID();
+    Serial.print("BMI160GenClass::i2c_init(): CHIP ID = 0x");
+    Serial.println(id, HEX);
+    Serial.println("BMI160GenClass::i2c_init()...done");
 #endif // DEBUG
 }
 
 int BMI160GenClass::i2c_xfer(uint8_t *buf, unsigned tx_cnt, unsigned rx_cnt)
 {
-  uint8_t *p;
+    uint8_t *p;
 
 #ifdef DEBUG
-  Serial.print("i2c_xfer(offs=0x");
-  Serial.print(*buf, HEX);
-  Serial.print(", tx=");
-  Serial.print(tx_cnt);
-  Serial.print(", rx=");
-  Serial.print(rx_cnt);
-  Serial.print("):");
+    Serial.print("i2c_xfer(offs=0x");
+    Serial.print(*buf, HEX);
+    Serial.print(", tx=");
+    Serial.print(tx_cnt);
+    Serial.print(", rx=");
+    Serial.print(rx_cnt);
+    Serial.print("):");
 #endif // DEBUG
 
-  Wire.beginTransmission(i2c_addr);
-  p = buf;
-  while (0 < tx_cnt) {
-    tx_cnt--;
-    Wire.write(*p++);
-  }
-  if( Wire.endTransmission() != 0 ) {
-      Serial.println("Wire.endTransmission() failed.");
-  }
-  if (0 < rx_cnt) {
-    Wire.requestFrom(i2c_addr, rx_cnt);
+    Wire.beginTransmission(i2c_addr);
     p = buf;
-    while ( Wire.available() && 0 < rx_cnt) {
-      rx_cnt--;
-#ifdef DEBUG
-      int t = *p++ = Wire.read();
-      Serial.print(" ");
-      Serial.print(t, HEX);
-#else
-      *p++ = Wire.read();;
-#endif // DEBUG
+    while (0 < tx_cnt)
+    {
+        tx_cnt--;
+        Wire.write(*p++);
     }
-  }
+    if (Wire.endTransmission() != 0)
+    {
+        Serial.println("Wire.endTransmission() failed.");
+    }
+    if (0 < rx_cnt)
+    {
+        Wire.requestFrom(i2c_addr, rx_cnt);
+        p = buf;
+        while (Wire.available() && 0 < rx_cnt)
+        {
+            rx_cnt--;
+#ifdef DEBUG
+            int t = *p++ = Wire.read();
+            Serial.print(" ");
+            Serial.print(t, HEX);
+#else
+            *p++ = Wire.read();
+            ;
+#endif // DEBUG
+        }
+    }
 
 #ifdef DEBUG
-  Serial.println("");
+    Serial.println("");
 #endif // DEBUG
 
-  return (0);
+    return (0);
 }
 
 //void BMI160GenClass::spi_init()
